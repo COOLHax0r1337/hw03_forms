581ffca649951d1f36ffd5b398bc17d8f848d82a
Philip Kurtakov <zj6lyp03r9oc@mail.ru>
2022-11-29 16:17:59 +0300

    tasks and tests fix

---
+++ yatube/posts/views.py
@@ -0,0 +1,104 @@
+from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404, render
+from . models import Group, Post, User
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from . forms import PostForm
+
+POST_LIMIT = 10
+
+
+def index(request):
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    posts = Post.objects.all().select_related('author')[:POST_LIMIT]
+    title = 'Последние обновления на сайте',
+    context = {
+        'posts': posts,
+        'title': title,
+        'page_obj': page_obj
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def group_posts(request, slug):
+    post_list = Post.objects.all()
+    group = get_object_or_404(Group, slug=slug)
+    posts = group.posts.all().select_related('author')[:POST_LIMIT]
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    title = 'Записи сообщества Лев Толстой – зеркало русской революции.'
+    context = {
+        'group': group,
+        'posts': posts,
+        'title': title,
+        'page_obj': page_obj
+    }
+    return render(request, 'posts/group_list.html', context)
+
+
+def profile(request, username):
+    author = get_object_or_404(User, username=username)
+    post_list = author.posts.all()
+    count = author.posts.count()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'count': count,
+        'author': author,
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/profile.html', context)
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    context = {
+        'post': post,
+    }
+    return render(request, 'posts/post_detail.html', context)
+
+@login_required
+def post_create(request):
+    form = PostForm(
+        request.POST or None,
+        files=request.FILES or None
+    )
+    if form.is_valid():
+        temp_form = form.save(commit=False)
+        temp_form.author = request.user
+        temp_form.save()
+        return redirect(
+            'posts:profile', temp_form.author
+        )
+    context = {
+        'form': form,
+    }
+    return render(request, 'posts/create_post.html', context)
+
+
+def post_edit(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    if post.author != request.user:
+        return redirect(
+            'posts:post_detail', post_id
+        )
+    form = PostForm(
+        request.POST or None,
+        files=request.FILES or None,
+        instance=post
+    )
+    if form.is_valid():
+        form.save()
+        return redirect(
+            'posts:post_detail', post_id
+        )
+    context = {
+        'form': form,
+        'is_edit': True,
+        'post': post
+    }
+    return render(request, 'posts/create_post.html', context)
